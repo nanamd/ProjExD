1b3e084fe4ab42b7c386cd1511a753ef2e5e86bc
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-14 17:51:13 +0900

    ランダムまで

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,36 +1,71 @@
+#<問題作り>
+#アルファベットのリストを作る
+#対象文字10文字をランダムで表示させる
+#対象文字から‐2文字させる(これを表示文字にする)
+#-2文字側を別の関数に入れる(これを削除された文字の1文字ずつ数える時に使う)
+#<回答作り>
+#
+#
+
+#ランダムを使うのでrandom変数呼び出し
 import random
-from time import altzone
-
-def main():
-    taisyou= AtoZ()
-    kaitou(taisyou)
-
-#対象文字のリスト(グローバル変数)
-
-def AtoZ():
-    mozi = ["A","B","C","D","E",
+#アルファベットのリスト
+mozi = ["A","B","C","D","E",
     "F","G","H","I","J","K","L",
     "M","N","O","P","Q","R","S",
     "T","U","O","P","Q","R","S",
     "T","U","V","W","X","Y","Z"]
-    print("対象文字：")
+#10文字をランダムで表示させる
+r=random.sample(mozi, 10)
+print(r)
+
+taisyou=r-2
+print(taisyou)
+
+
+#def taisyou():
+  #  mozi = ["A","B","C","D","E",
+  #  "F","G","H","I","J","K","L",
+  #  "M","N","O","P","Q","R","S",
+   # "T","U","O","P","Q","R","S",
+   # "T","U","V","W","X","Y","Z"]
+  #  r=random.sample(mozi)
+   # return 
+#print("対象文字："+random.sample(taisyou))
+#from time import altzone
+
+#def main():
+ #  taisyou= AtoZ()
+   #kaitou(taisyou)
+
+#対象文字のリスト(グローバル変数)
+
     #AtoZのリストから10文字出す
-    r=random.randint(9)
-    print(mozi[r])
-    return mozi[r]
+
+#def main():
+
+#    mozi = ["A","B","C","D","E",
+ #   "F","G","H","I","J","K","L",
+  #  "M","N","O","P","Q","R","S",
+   # "T","U","O","P","Q","R","S",
+    #"T","U","V","W","X","Y","Z"]
+    #AtoZのリストから10文字出す
+   # print("対象文字："random.sample(mozi,9))
+    
+    
 
 #表示文字のリスト()
 #対象文字rから-2した文字列
-hyouzi=mozi[r-2]
-print(hyouzi)
+#hyouzi=mozi[r-2]
+#print(hyouzi)
 
 
-def kaitou(taisyou):
-    ans = input("欠損文字はいくつ:")
-    if ans in taisyou:
-        print("正解")
-    else:
-        print("間違い")
+#def kaitou(taisyou):
+   # ans = input("欠損文字はいくつ:")
+    #if ans in taisyou:
+    #    print("正解")
+    #else:
+     #   print("間違い")
 
-if __name__=="__main__":
-    main()
+#if __name__=="__main__":
+ #   main()
