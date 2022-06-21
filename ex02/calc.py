477cfc11c99effa574268b31cf05c00aa1792424
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-21 16:26:50 +0900

    コードをきれいに

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,52 +1,83 @@
-#tkを引っ張ってくる
+# Libraries Import　引っ張ってくる
+from logging import RootLogger, root
 import tkinter as tk
-#import tkinter.messagebox as tkm
+from tkinter import ttk
+ 
+#ボタンの種類
+# Define
+BUTTON = [
+    ['', 'C', 'AC', '/'],
+    ['7', '8', '9', '*'],
+    ['4', '5', '6', '-'],
+    ['1', '2', '3', '+'],
+    ['00', '0', '.', '=']
+]
+ 
+SYMBOL = ['+', '-', '*', '/']
+ 
+# 電卓の機能
+class Tuika(object):
+    def __init__(self, root=None):
+        # Define
+        self.calc_str = '' # 計算用の文字列
+ 
+        # Frame Setting
+        calc_frame = ttk.Frame(root, width=300, height=100) # 計算式と結果用のFrame
+        calc_frame.propagate(False) # サイズが固定される
+        calc_frame.pack(side=tk.TOP, padx=10, pady=20) # 余白の設定
+        button_frame = ttk.Frame(root, width=300, height=400) # 計算ボタン用のFrame
+        button_frame.propagate(False) # サイズが固定される
+        button_frame.pack(side=tk.BOTTOM) # 余白の設定
+ 
+        # Parts Setting
+        self.calc_var = tk.StringVar() # 計算式用の動的変数
+        self.ans_var = tk.StringVar() # 結果用の動的変数
+        calc_label = tk.Label(calc_frame, textvariable=self.calc_var, font=("",20)) # 計算式用のLabel
+        ans_label = tk.Label(calc_frame, textvariable=self.ans_var, font=("",15)) # 結果用のLabel
+        calc_label.pack(anchor=tk.E) # 右揃えで配置
+        ans_label.pack(anchor=tk.E) # 右揃えで配置
+ 
+ #ボタンの設定
+        for y, row in enumerate(BUTTON, 1): # Buttonの配置
+            for x, num in enumerate(row):
+                #ボタンの色や大きさ等の変更
+                button = tk.Button(button_frame, text=num, font=('', 20), width=6, height=3, bg="blue",fg="red")
+                button.grid(row=y, column=x) # 列や行を指定して配置
+                button.bind('<Button-1>', self.click_button) # Buttonが押された場合
 
-#button_click関数
-def button_click(event):
-    btn = event.widget
-    num = btn["text"]
-    #tkm.showinfo(txt, f"{txt}ボタンクリックされました")
-    entry.insert(tk.END,num)
+    #ボタンが押された時の設定
+    def click_button(self, event):
+        check = event.widget['text'] # 押したボタンのCheck
 
-def click_equal(event):
-    eqn = entry.get()
-    res = eval(eqn)
-    entry.delete(0, tk.END)
-    entry.insert(tk.END, str(res))
-
-#name関数
-if __name__ == "__main__":
-    #ウィンドウ設定
+ #四則演算の設定
+        if check == '=': # イコールの場合
+            if self.calc_str[-1:] in SYMBOL: # 記号の場合、記号よりも前で計算
+                self.calc_str = self.calc_str[:-1]
+ 
+            res = '= ' + str(eval(self.calc_str)) # eval関数の利用
+            self.ans_var.set(res)
+        elif check == 'AC': # クリアの場合
+            self.calc_str = ''
+            self.ans_var.set('')
+        elif check == 'C': # バックの場合
+            self.calc_str = self.calc_str[:-1]
+        elif check in SYMBOL: # 記号の場合
+            if self.calc_str[-1:] not in SYMBOL and self.calc_str[-1:] != '':
+                self.calc_str += check
+            elif self.calc_str[-1:] in SYMBOL: # 記号の場合、入れ替える
+                self.calc_str = self.calc_str[:-1] + check
+        else: # 数字などの場合
+            self.calc_str += check
+ 
+        self.calc_var.set(self.calc_str)
+    
+ 
+if __name__ == '__main__':
+    # Window Setting
     root = tk.Tk()
-    root.title("tk")
-    #サイズ指定なし→常にピッタリになる為
-    #root.geometry("300x500")
-
-    entry = tk.Entry(root,
-                    justify="right",
-                    width=10,
-                    font=("Times New Roman", 30))
-    entry.grid(row=0, column=1, columnspan=4)
-
-    r,c=1,1
-    #ボタンの種類
-    for i, num in enumerate([9,8,7,6,5,4,3,2,1,0,"+"], 1):
-        #ボタンの大きさとフォントの種類
-        btn = tk.Button(root,text=num, font=("Times New Roman", 30))
-        btn.bind("<1>", button_click)
-        btn.grid(row=r, column=c, padx=10, pady=10)
-        if i%3 == 0:
-            r += 1
-            c = 0
-        c += 1
-
-    btn = tk.Button(root,
-                    text="=",
-                    font=("Times New Roman",30)
-                    )
-    btn.bind("<1>",click_equal)
-    btn.grid(row=r,column=c,padx=10,pady=10)
-
-#ウィンドウを表示
-root.mainloop()
+    # Window size non resizable
+    root.resizable(width=False, height=False)
+    root.title('三島電卓') # Window のタイトル
+    Tuika(root)
+    # Display
+    root.mainloop() # Window をループで回すことで Widgit に対応できるようになる
