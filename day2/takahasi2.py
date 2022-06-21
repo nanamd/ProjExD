d6093c6f76e9adc0c0064fcdb699ccddb6259d45
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-21 14:47:39 +0900

    サイズ変更

--- day2/takahasi2.py
+++ day2/takahasi2.py
@@ -21,10 +21,10 @@ def button_click(event):
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("tk")
-    root.geometry("300x450")
+    root.geometry("450x800")
     r, c = 1, 1
     for i, num in enumerate(range(9, -1, -1), 1):
-        btn = tk.Button(root,text=num,command=button_click, font=("Times New Roman", 50))
+        btn = tk.Button(root,text=num,command=button_click, font=("Times New Roman", 30))
         btn.bind("<1>", button_click)
         btn.grid(row=r, column=c, padx=3, pady=3)
         if i%3 == 0:
@@ -32,37 +32,37 @@ if __name__ == "__main__":
             c = 0
         c += 1
 
-    btn = tk.Button(root,text="+", font=("Times New Roman", 50))
+    btn = tk.Button(root,text="+", font=("Times New Roman", 30))
     btn.bind("<1>", button_click)
     btn.grid(row=1, column=4, padx=3,pady=3)
 
-    btn = tk.Button(root,text="=", font=("Times New Roman", 50))
+    btn = tk.Button(root,text="=", font=("Times New Roman", 30))
     btn.bind("<1>", click_equal)
     btn.grid(row=4, column=3, padx=3,pady=3)
-    btn = tk.Button(root,text="C", font=("Times New Roman", 50))
+    btn = tk.Button(root,text="C", font=("Times New Roman", 30))
     btn.bind("<1>", button_click)
     btn.grid(row=4, column=2, padx=3,pady=3)
 
-    btn = tk.Button(root,text="-", font=("Times New Roman", 50))
+    btn = tk.Button(root,text="-", font=("Times New Roman", 30))
     btn.bind("<1>", button_click)
     btn.grid(row=2, column=4, padx=4,pady=3)
 
-    btn = tk.Button(root,text="*", font=("Times New Roman", 50))
+    btn = tk.Button(root,text="*", font=("Times New Roman", 30))
     btn.bind("<1>", button_click)
     btn.grid(row=3, column=4, padx=4,pady=3)
 
-    btn = tk.Button(root,text="/", font=("Times New Roman", 50))
+    btn = tk.Button(root,text="/", font=("Times New Roman", 30))
     btn.bind("<1>", button_click)
     btn.grid(row=4, column=4, padx=4,pady=3)
-    btn = tk.Button(root,text="00", font=("Times New Roman", 40))
+    btn = tk.Button(root,text="00", font=("Times New Roman", 30))
     btn.bind("<1>", button_click)
     btn.grid(row=5, column=4, padx=4,pady=3)
 
-    btn = tk.Button(root,text="B", font=("Times New Roman", 40))
+    btn = tk.Button(root,text="B", font=("Times New Roman", 30))
     btn.bind("<1>", button_click)
     btn.grid(row=5, column=3, padx=4,pady=3)
 
-entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 50))
+entry = tk.Entry(root, justify="right", width=10, font=("Times New Roman", 30))
 entry.grid(row=0, column=1, columnspan=4)
 
 root.mainloop()
