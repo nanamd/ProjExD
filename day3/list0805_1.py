cdaa189bf4116eea881cfa8dbebb563ee1739e8e
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-28 15:26:33 +0900

    k

---
+++ day3/list0805_1.py
@@ -0,0 +1,26 @@
+import tkinter
+
+key=""
+def key_down(e):
+    global key
+    key=e.keysym
+
+def key_up(e):
+    global key
+    key = ""
+
+mx = 1
+my = 1
+
+def main_proc():
+    global mx, my
+    if key == "Up" and maze[my-1][mx]==0:
+        my = my -1
+    if key == "Down" and maze[my+1][mx]==0:
+        my = my +1
+    if key == "Left" and maze[my][mx-1]==0:
+        my = my -1
+    if key == "Right" and maze[my][mx+1]==0:
+        my = my +1
+    canvas.coords("MYCHR",mx*)
+    
