44de8728442c915b71c18f78fd59e8244f9d6d4b
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-28 16:29:38 +0900

    不要なもの削除

--- ex03/maze.py
+++ ex03/maze.py
@@ -5,31 +5,31 @@ import random
 def key_down(event):
     global key
     key=event.keysym
-    #print(f"{key}が押され毎s多")
+    
 
 def key_up(event):
     global key
     key=""
 
 def main_proc():
-    global cx, cy, mx, my, key, n, tori
+    global cx, cy, key, mx, my, n, tori
     #key:押されているキーkey/値：移動幅リスト[x,y]
 
     delta = {
-        #"" :[0,0],
             "Up":[0,-1],
             "Down":[0,+1],
             "Left":[-1,0],
             "Right":[+1,0],
             }
     try:
-        if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0:#移動先が床なら
-            my,mx = my+delta[key][1], mx+delta[key][0]
+        if maze_bg[my+delta[key][1]][mx+delta[key][0]]==0:
+            mx += delta[key][0]
+            my += delta[key][1]
     except:
         pass
-    #cx,cy = cx+delta[key][0], cy+delta[key][1]
     
-    cx,cy = mx*100+50, my*100+50
+    
+    cx,cy = 100*mx+50, 100*my+50
     canvas.coords("tori",cx,cy)
 
     if n>1:
@@ -39,7 +39,6 @@ def main_proc():
     n += 1
 
     root.after(100,main_proc)
-    #print(f"{key}が押されました")
     
 def img_select():
     global img
@@ -56,19 +55,17 @@ if __name__ == "__main__":
                         bg="black")
     canvas.pack()
 
-    maze_bg = mm.make_maze(13,7)
-    #1:壁/0;床
-    #canvasにmaze_bgを書く
+    maze_bg = mm.make_maze(13,7)#1:壁/0;床 #canvasにmaze_bgを書く
     mm.show_maze(canvas, maze_bg)
-    print(maze_bg)
+
+    #print(maze_bg)
 
     tori = tk.PhotoImage(file=img_select())
     mx,my=1,1
-    cx,cy = mx*100+50, my*100+50
+    cx,cy = 100*mx+50, 100*my+50
     canvas.create_image(cx,cy,image=tori,tag="tori")
 
     key = ""
-
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>", key_up)
     root.bind("<Double-1>",img_select)
