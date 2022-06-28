148bf0344b870b8775aec31981df22d07adc325e
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-28 13:36:27 +0900

    リアルタイムキー入力

---
+++ day3/list0803_2.py
@@ -0,0 +1,20 @@
+from cProfile import label
+from logging import root
+import tkinter
+
+key=""
+def key_down(e):
+    global key 
+    key = e.keysym
+
+def main_proc():
+    label["text"]=key
+    root.after(100,main_proc)
+
+root = tkinter.Tk()
+root.title("リアルタイムキー入力")
+root.bind("<KeyPress>",key_down)
+label = tkinter.Label(font=("Time New Roman",80))
+label.pack()
+main_proc()
+root.mainloop()
