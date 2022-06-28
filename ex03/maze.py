70bdc9854285804dc86be3c79bb03a6497bfc4e1
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-28 14:19:03 +0900

    2

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,4 +4,8 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+
+    canvas = tk.Canvas(root,width=1500,height=900, bg="black")
+    canvas.pack()
+
     root.mainloop()
