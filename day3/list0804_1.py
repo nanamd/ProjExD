226bbefedfb8e9d20dbd76907ec02e08fe2dfb5e
Mishima <c0a2111648@edu.teu.ac.jp>
2022-06-28 14:17:00 +0900

    mada

---
+++ day3/list0804_1.py
@@ -0,0 +1,22 @@
+from logging import root
+import tkinter
+
+root = tkinter.Tk()
+root.title("迷路の表示")
+canvas = tkinter.Canvas(width=800,height=560,
+                        bg="white")
+
+canvas.pack()
+maze = [
+    [1,1,1,1,1,1,1,1,1,1],
+    [1,0,0,0,0,0,1,0,0,1],
+    [1,0,1,1,0,0,1,0,0,1],
+    [1,0,0,1,0,0,0,0,0,1],
+    [1,0,0,1,1,1,1,1,0,1],
+    [1,0,0,0,0,0,0,0,0,1],
+    [1,1,1,1,1,1,1,1,1,1]
+    ]
+
+for y in range(7):
+    for x in range(10):
+        if maze[y][x] == 1:
