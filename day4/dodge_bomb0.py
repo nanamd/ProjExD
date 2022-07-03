215cecbe6b19b7420ed6bfa9f00c0ed96b1a01ae
Mishima <c0a2111648@edu.teu.ac.jp>
2022-07-03 13:20:14 +0900

    練習３

--- day4/dodge_bomb0.py
+++ day4/dodge_bomb0.py
@@ -1,7 +1,3 @@
-
-from ast import While
-import py_compile
-import re
 import pygame as pg
 import sys
 
@@ -10,18 +6,28 @@ def main():
     screen = pg.display.set_mode((1200,650))
     sc_rect = screen.get_rect()                     #画面用rect
     bg_img = pg.image.load("fig/pg_bg.jpg")         #背景画像用のsurface
-    bg_rect = bg_img.get_rect()                     #背景用rect
-    screen.blit(bg_img, bg_rect)                    #背景用surfaceを画面用surfaceに貼り付ける    
-    pg.display.update()  #画面の更新
+    bg_rect = bg_img.get_rect()                     #背景用rect    
     clock = pg.time.Clock() #表示時間の設定
-    clock.tick(1000) #1秒に1000画像を表示する(ぬるぬる動く)
+    
+
+    #こうかとん磔
+    tori_img = pg.image.load("fig/6.png")                   #こうかとん画像用のsurface
+    #rotozoomは画像の拡大縮小回転(画像名,回転,拡縮)
+    tori_img=  pg.transform.rotozoom(tori_img, 0, 2.0)      #こうかとん画像の拡大
+    tori_rect = tori_img.get_rect()                         #こうかとん画像用のrect
+    tori_rect.center =900, 400                              #こうかとんの中心を900,400に指定
+    #blit(画像名,サイズ)
+    screen.blit(tori_img, tori_rect) 
 
     while True:
         screen.blit(bg_img,bg_rect) #スクリーンに背景画像を合成(サイズ通りに)
+        screen.blit(tori_img,tori_rect)
         for event in pg.event.get(): #イベント全てを格納する箱
             #ウィンドウの閉じるボタンを押したとき
             if event.type == pg.QUIT:
                 return
+        pg.display.update()
+        clock.tick(1000) #1秒に1000画像を表示する(ぬるぬる動く)
 
 
 if __name__ == "__main__":
