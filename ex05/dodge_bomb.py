0df1567d8d0bddab2a0951e0fa891d4eec2e3aae
Mishima <c0a2111648@edu.teu.ac.jp>
2022-07-12 17:00:11 +0900

    終了もう無理じゃね？

--- ex05/dodge_bomb.py
+++ ex05/dodge_bomb.py
@@ -78,29 +78,15 @@ def main():
     clock = pg.time.Clock()
 
     # 練習1：スクリーンと背景画像
-    #pg.display.set_caption("逃げろ！こうかとん")
-    #screen_sfc = pg.display.set_mode((1600, 900)) # Surface
-    #screen_rct = screen_sfc.get_rect()            # Rect
-    #bgimg_sfc = pg.image.load("fig/pg_bg.jpg")    # Surface
-    #bgimg_rct = bgimg_sfc.get_rect()              # Rect
-    #screen_sfc.blit(bgimg_sfc, bgimg_rct)
-    scr = Screen("逃げろ！こうかとん",(1600,900),"fig/pg_bg.jpg")#コメントアウトの6文が一行に
+    
+    scr = Screen("逃げろ！こうかとん",(1200,650),"fig/pg_bg.jpg")#コメントアウトの6文が一行に
 
     # 練習3：こうかとん
-    #kkimg_sfc = pg.image.load("fig/6.png")    # Surface
-    #kkimg_sfc = pg.transform.rotozoom(kkimg_sfc, 0, 2.0)  # Surface
-    #kkimg_rct = kkimg_sfc.get_rect()          # Rect
-    #kkimg_rct.center = 900, 400
+    
     kkt = Bird("fig/6.png", 2.0, (900,400))
 
     # 練習5：爆弾
-    #bmimg_sfc = pg.Surface((20, 20)) # Surface
-    #bmimg_sfc.set_colorkey((0, 0, 0)) 
-    #pg.draw.circle(bmimg_sfc, (255, 0, 0), (10, 10), 10)
-    #bmimg_rct = bmimg_sfc.get_rect() # Rect
-    #bmimg_rct.centerx = random.randint(0, screen_rct.width)
-    #bmimg_rct.centery = random.randint(0, screen_rct.height)
-    #vx, vy = +1, +1 # 練習6
+    # 練習6
     bkb = Bomb((255,0,0),10,(+1,+1),scr)
 
     while True:
@@ -113,18 +99,9 @@ def main():
             if event.type == pg.QUIT: return
 
         # 練習4
-        #key_states = pg.key.get_pressed() # 辞書
-        #if key_states[pg.K_UP]    == True: kkimg_rct.centery -= 1
-        #if key_states[pg.K_DOWN]  == True: kkimg_rct.centery += 1
-        #if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx -= 1
-        #if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx += 1
+        
         # 練習7
-        #if check_bound(kkimg_rct, screen_rct) != (1, 1): # 領域外だったら
-         #   if key_states[pg.K_UP]    == True: kkimg_rct.centery += 1
-          #  if key_states[pg.K_DOWN]  == True: kkimg_rct.centery -= 1
-           # if key_states[pg.K_LEFT]  == True: kkimg_rct.centerx += 1
-            #if key_states[pg.K_RIGHT] == True: kkimg_rct.centerx -= 1
-        #screen_sfc.blit(kkimg_sfc, kkimg_rct)
+        
 
         kkt.update(scr) #上の長いやつがこれになる
 
